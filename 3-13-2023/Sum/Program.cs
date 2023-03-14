5f980e7087f595d8072710b59fcb7010d07d5604
KevinCornell <kevdcornell@gmail.com>
2023-03-14 14:36:40 -0400

    Sum Coding Challenge

---
+++ 3-13-2023/Sum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static void Main()
+    {
+        //Examples
+        Console.WriteLine("Sum(3,2) = {0}", Sum(3, 2));
+        Console.WriteLine("Sum(-3,-6) = {0}", Sum(-3, -6));
+        Console.WriteLine("Sum(7,3) = {0}", Sum(7, 3));
+    }
+}

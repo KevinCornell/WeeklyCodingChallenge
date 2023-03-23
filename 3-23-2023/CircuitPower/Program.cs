f2f5ce62cc59ebaac52141ec7a443dd3ce577700
KevinCornell <kevdcornell@gmail.com>
2023-03-23 11:15:25 -0400

    Circuit Power Coding Challenge

---
+++ 3-23-2023/CircuitPower/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static int CircuitPower(int v, int c)
+    {
+        return v * c;
+    }
+
+    public static void Main()
+    {
+        //Examples
+        Console.WriteLine("CircuitPower(230,10) = {0}", CircuitPower(230, 10));
+        Console.WriteLine("CircuitPower(110,3) = {0}", CircuitPower(110, 3));
+        Console.WriteLine("CircuitPower(480,20) = {0}", CircuitPower(480, 20));
+    }
+}
